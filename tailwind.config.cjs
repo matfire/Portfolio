2952e3ed6deaef78867a06f5340bb539c908d53a
Matteo Gassend <mgassend@gmail.com>
2023-05-26 09:01:25 +0200

    wip

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Altinn-Din", ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [require("@tailwindcss/forms"), require('tailwind-scrollbar'),
   ],
