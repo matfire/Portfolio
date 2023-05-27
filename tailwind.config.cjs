37beba12d0417710f713839ed4d045b22ec89244
Matteo Gassend <mgassend@gmail.com>
2023-05-27 14:15:16 +0200

    add: header + configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,28 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+        '2xl': '1536px' // => @media (min-width: 1536px) { ... }
+
+      },
       fontFamily: {
         sans: ["Altinn-Din", ...defaultTheme.fontFamily.sans],
+      },
+      
+      backgroundColor: {
+        primary: {
+          lighter: '#dbe6e7',
+          "DEFAULT": '#F2F6FC',
+          darker: '#374053'
+        },
       }
     },
   },
