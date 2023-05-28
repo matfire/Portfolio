ba154569c1af80a2b4f1ccdcd7f9b784bc327df6
Matteo Gassend <mgassend@gmail.com>
2023-05-28 17:11:13 +0200

    add: index page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,29 +5,30 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
-      screens: {
-        'sm': '640px',
-        // => @media (min-width: 640px) { ... }
-        'md': '768px',
-        // => @media (min-width: 768px) { ... }
-        'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
-        'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-        '2xl': '1536px' // => @media (min-width: 1536px) { ... }
-
-      },
       fontFamily: {
         sans: ["Altinn-Din", ...defaultTheme.fontFamily.sans],
       },
-      
-      backgroundColor: {
+      colors: {
         primary: {
           lighter: '#dbe6e7',
           "DEFAULT": '#F2F6FC',
           darker: '#374053'
         },
-      }
+      },
+      backgroundColor: {
+        primary: {
+          lighter: '#dfe5ef',
+          "DEFAULT": '#F2F6FC',
+          darker: '#374053'
+        },
+      },
+      textColor: {
+        primary: {
+          lighter: '#dfe5ef',
+          "DEFAULT": '#F2F6FC',
+          darker: '#374053'
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/forms"), require('tailwind-scrollbar'),
