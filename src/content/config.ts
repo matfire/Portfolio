f21b203aad702bf0f81ea6d9d600393067d85d38
matfire <mgassend@gmail.com>
2023-08-04 10:19:28 +0000

    add: simple-local-notifications project

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,45 +1,46 @@
-import { defineCollection, z } from "astro:content"
+import { defineCollection, z } from "astro:content";
 
 const projectsCollection = defineCollection({
-    schema: ({image}) => z.object({
-        title: z.string(),
-        summary: z.string(),
-        date: z.date().transform((v) => new Date(v)),
-        stack: z.array(z.string()),
-        image: image(),
-        productionUrl: z.string().optional(),
-        epitech: z.boolean().default(false),
-        personal: z.boolean().default(false),
-        githubUrl: z.string().optional()
-    })
-})
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      summary: z.string(),
+      date: z.date().transform((v) => new Date(v)),
+      stack: z.array(z.string()),
+      productionUrl: z.string().optional(),
+      epitech: z.boolean().default(false),
+      personal: z.boolean().default(false),
+      githubUrl: z.string().optional(),
+    }),
+});
 
 const educationCollection = defineCollection({
-    schema: ({image}) => z.object({
-        title:z.string(),
-        begin: z.date().transform((v) => new Date(v)),
-        graduation: z.date().transform((v) => new Date(v)),
-        schoolName: z.string(),
-        country: z.string(),
-        city: z.string(),
-        logo: image()
-    })
-})
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      begin: z.date().transform((v) => new Date(v)),
+      graduation: z.date().transform((v) => new Date(v)),
+      schoolName: z.string(),
+      country: z.string(),
+      city: z.string(),
+      logo: image(),
+    }),
+});
 
 const experienceCollection = defineCollection({
-    schema: z.object({
-        companyName: z.string(),
-        begin: z.date().transform((v) => new Date(v)),
-        end: z.date().transform((v) => new Date(v)),
-        role: z.string(),
-        skills: z.array(z.string()),
-        des: z.array(z.string()),
-        type: z.enum(["internship", "fixed-term contract", "permanent contract"])
-    })
-})
+  schema: z.object({
+    companyName: z.string(),
+    begin: z.date().transform((v) => new Date(v)),
+    end: z.date().transform((v) => new Date(v)),
+    role: z.string(),
+    skills: z.array(z.string()),
+    des: z.array(z.string()),
+    type: z.enum(["internship", "fixed-term contract", "permanent contract"]),
+  }),
+});
 
 export const collections = {
-    'projects': projectsCollection,
-    'education': educationCollection,
-    'experience': experienceCollection,
-}
+  projects: projectsCollection,
+  education: educationCollection,
+  experience: experienceCollection,
+};
---
+++ src/content/projects/simple-local-notifications.md
@@ -0,0 +1,10 @@
+---
+title: "Simple-Local-Notifications"
+summary: "basic flutter plugin to send local notifications"
+date: 2023-08-04
+stack:
+    - dart
+    - kotlin
+githubUrl: https://github.com/matfire/simple_local_notifications
+productionUrl: https://pub.dev/packages/simple_local_notifications
+---
