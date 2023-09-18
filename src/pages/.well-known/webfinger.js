4671e052c70c329ca94bb7ca6f1cf856ff64b79a
matfire <mgassend@gmail.com>
2023-09-18 11:21:43 +0200

    add: webfinger endpoint

---
+++ src/pages/.well-known/webfinger.js
@@ -0,0 +1,18 @@
+export async function get() {
+  return {
+    body: JSON.stringify({
+        "subject": "acct:mgassend@hachyderm.io", 
+      "aliases": [
+        "https://hachyderm.io/@mgassend", 
+        "https://hachyderm.io/users/mgassend"
+      ], 
+      "links": 
+      [
+        { "rel": "http://webfinger.net/rel/profile-page", "type": "text/html", "href": "https://hachyderm.io/@mgassend" }, 
+        { "rel": "self", "type": "application/activity+json", "href": "https://hachyderm.io/users/mgassend" }, 
+        { "rel": "http://ostatus.org/schema/1.0/subscribe", "template": "https://hachyderm.io/authorize_interaction?uri={uri}" }
+      ] 
+    }
+    ),
+  }
+}
