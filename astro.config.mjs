1aa59919b9000a6213be905894737c8f89227ab7
matfire <mgassend@gmail.com>
2023-09-19 10:55:31 +0200

    add: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig, sharpImageService } from "astro/config";
 
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
@@ -8,12 +7,11 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import robotsTxt from "astro-robots-txt";
-
 import remarkEmbedder from "@remark-embedder/core";
-import {
-  CodeSandboxTransformer,
-  YoutubeTransformer,
-} from "./src/utils/embedTransformers";
+import { CodeSandboxTransformer, YoutubeTransformer } from "./src/utils/embedTransformers";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,18 +19,18 @@ export default defineConfig({
   site: "https://matteogassend.com",
   adapter: vercel(),
   image: {
-    service: sharpImageService(),
+    service: sharpImageService()
   },
   markdown: {
-    remarkPlugins: [
-      [
-        remarkEmbedder,
-        { transformers: [CodeSandboxTransformer, YoutubeTransformer] },
-      ],
-    ],
+    remarkPlugins: [[remarkEmbedder, {
+      transformers: [CodeSandboxTransformer, YoutubeTransformer]
+    }]]
   },
-  integrations: [
-    tailwind(),
-    robotsTxt({ policy: [{ userAgent: "*", allow: "/" }] }),
-  ],
+  integrations: [tailwind(), robotsTxt({
+    policy: [{
+      userAgent: "*",
+      allow: "/"
+    }],
+    sitemap: `https://matteogassend.com/sitemap-index.xml`
+  }), sitemap()]
 });
