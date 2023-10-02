1c07c201d3f80fcd1ebc4fa2821701be648953f9
matfire <mgassend@gmail.com>
2023-10-02 14:20:08 +0200

    fix: cross-origin isolated stackblitz

--- src/utils/embedTransformers.ts
+++ src/utils/embedTransformers.ts
@@ -37,6 +37,6 @@ export const StackblitzTransformer = {
     return ["stackblitz.com"].includes(host)
   },
   getHTML (url: string) {
-    return `<iframe src="${url}" style="width:100%; height:500px;"></iframe>`
+    return `<iframe src="${url}" style="width:100%; height:500px;" allow="cross-origin-isolated"></iframe>`
   }
 }
