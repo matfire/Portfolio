4bfbb5fa3c7ff990258c12630cf6e86f277aa362
matfire <mgassend@gmail.com>
2024-01-15 16:40:16 +0100

    fix: webfinger returns response

--- src/pages/.well-known/webfinger.js
+++ src/pages/.well-known/webfinger.js
@@ -1,18 +1,17 @@
 export async function GET() {
-  return {
-    body: JSON.stringify({
-        "subject": "acct:mgassend@hachyderm.io", 
+  return new Response(JSON.stringify(
+    {
+      "subject": "acct:mgassend@hachyderm.io",
       "aliases": [
-        "https://hachyderm.io/@mgassend", 
+        "https://hachyderm.io/@mgassend",
         "https://hachyderm.io/users/mgassend"
-      ], 
-      "links": 
-      [
-        { "rel": "http://webfinger.net/rel/profile-page", "type": "text/html", "href": "https://hachyderm.io/@mgassend" }, 
-        { "rel": "self", "type": "application/activity+json", "href": "https://hachyderm.io/users/mgassend" }, 
-        { "rel": "http://ostatus.org/schema/1.0/subscribe", "template": "https://hachyderm.io/authorize_interaction?uri={uri}" }
-      ] 
+      ],
+      "links":
+        [
+          { "rel": "http://webfinger.net/rel/profile-page", "type": "text/html", "href": "https://hachyderm.io/@mgassend" },
+          { "rel": "self", "type": "application/activity+json", "href": "https://hachyderm.io/users/mgassend" },
+          { "rel": "http://ostatus.org/schema/1.0/subscribe", "template": "https://hachyderm.io/authorize_interaction?uri={uri}" }
+        ]
     }
-    ),
-  }
+  ))
 }
