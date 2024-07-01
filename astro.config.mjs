0adb3551984e1b884beb7e2a1cb4f31b52a23293
Matteo Gassend <mgassend@gmail.com>
2024-07-01 11:25:44 +0200

    add: switch to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,25 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import robotsTxt from "astro-robots-txt";
 import remarkEmbedder from "@remark-embedder/core";
 import { CodeSandboxTransformer, YoutubeTransformer, StackblitzTransformer } from "./src/utils/embedTransformers";
 
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
 import icon from "astro-icon";
 
 // https://astro.build/config
 import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   site: "https://matteogassend.com",
-  adapter: cloudflare(),
+  adapter: vercel(),
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp'
